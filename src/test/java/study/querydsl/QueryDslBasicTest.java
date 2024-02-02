2207aa4f83a69e6112398029c977d991605c3f4b
DrumJ <tmdgh717@naver.com>
2024-02-02 22:40:47 +0900

    feat. 정렬

--- src/test/java/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -105,4 +105,32 @@ public class QueryDslBasicTest {
                 .from(member).fetchOne();
         
     }
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    void sort() throws Exception {
+        //데이터 추가
+        em.persist(new Member(null,100));
+        em.persist(new Member("member5",100));
+        em.persist(new Member("member6",100));
+
+        List<Member> fetch = query.selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(),
+                        member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = fetch.get(0);
+        Member member6 = fetch.get(1);
+        Member memberNull = fetch.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isEqualTo(null);
+
+    }
 }
