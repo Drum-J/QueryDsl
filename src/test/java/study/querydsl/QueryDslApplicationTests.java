d1750ecaad37aad3fdc06f3cd2814b9c478592e8
Drum-J <tmdgh717@naver.com>
2024-02-02 15:33:53 +0900

    프로젝트 생성

---
+++ src/test/java/study/querydsl/QueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
