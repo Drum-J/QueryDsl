187e6c0fae3706eb0c10009e02073f9f530d0d5c
DrumJ <tmdgh717@naver.com>
2024-02-02 21:36:52 +0900

    feat . JPQL 과 QueryDsl 비교

---
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -0,0 +1,65 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class QueryDslBasicTest {
+
+    @Autowired EntityManager em;
+
+    JPAQueryFactory query;
+    @BeforeEach
+    public void before() {
+        query = new JPAQueryFactory(em); // 필드 레벨에서 처리해도 괜찮다고 한다.
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 20, teamA);
+        Member member2 = new Member("member2", 21, teamA);
+
+        Member member3 = new Member("member3", 22, teamB);
+        Member member4 = new Member("member4", 23, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    void startJPQL() throws Exception {
+        //member1을 찾아라.
+        Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void startQueryDsl() throws Exception {
+        QMember m = new QMember("m"); // QMember 를 구분하는 이름이라고 한다. 이렇게는 잘 안쓰고 QMember.member 를 자주 사용한다고 함.
+
+        Member findMember = query.selectFrom(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
